57d1004a72bb11d15f15f71a89cde5e5cb77de84
Luca Vicidomini <lvicidomini@unisa.it>
2016-11-14 12:44:32 +0100

    Login page localization en, it

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,4 +16,27 @@ return [
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+	'login' => [
+		'meta_title' => 'Login',
+		'title' => 'Welcome',
+		'login_card' => [
+			'title' => 'Log in',
+			'message' => 'If you are already registered, log in using your username and password.',
+			'action' => 'Log in',
+			'email' => 'E-mail',
+			'password' => 'Password',
+			'remember_me' => 'Remember me',
+		],
+		'register_card' => [
+			'title' => 'New user?',
+			'message' => 'Join us and start working on Open Data: create, publish and discuss them.',
+			'action' => 'Register now',
+		],
+		'password_reset_card' => [
+			'title' => 'Forgot password?',
+			'message' => 'We can e-mail you a link that will help you in resetting your password.',
+			'action' => 'Reset password',
+		],
+	],
+
 ];
---
+++ resources/lang/it/auth.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Nome utente o password non validi.',
+    'throttle' => 'Hai effettuato troppi tentativi di accesso. Potrai accedere nuovamente tra :seconds secondi.',
+
+    'login' => [
+	    'meta_title' => 'Login',
+	    'title' => 'Benvenuto',
+	    'login_card' => [
+		    'title' => 'Log in',
+		    'message' => 'Se sei registrato, puoi accedere utilizzando nome utente e password in tuo possesso.',
+		    'action' => 'Accedi',
+		    'email' => 'E-mail',
+		    'password' => 'Password',
+		    'remember_me' => 'Ricordami',
+	    ],
+	    'register_card' => [
+		    'title' => 'Nuovo utente?',
+		    'message' => 'Unisciti a noi e lavora con gli Open Data: crea, pubblica e discuti sulla nostra piattaforma.',
+		    'action' => 'Registrati',
+	    ],
+	    'password_reset_card' => [
+		    'title' => 'Password dimenticata?',
+		    'message' => 'Possiamo inviarti una e-mail contenente un collegamento con cui procedere a reimpostare la password.',
+		    'action' => 'Reimposta password',
+	    ],
+    ],
+
+];
