2fe16e27ecffefd3ed48b150c62c8d971084906c
Luca Vicidomini <lvicidomini@unisa.it>
2016-11-14 12:56:56 +0100

    Pagination, password localization it

---
+++ resources/lang/it/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La password deve essere lunga almeno sei caratteri e corrispondere al campo verifica.',
+    'reset' => 'La password è stata reimpostata.',
+    'sent' => 'A breve riceverai una email con il link per reimpostare la password.',
+    'token' => 'Il link per la reimpostazione della password non è valido o è scaduto.',
+    'user' => 'L\'indirizzo email non è stato trovato nel nostro database.',
+
+];
