a80328f411f50b81ea0ac7fd7c9114c1b642bfd3
Luca Vicidomini <lvicidomini@unisa.it>
2016-11-21 11:23:21 +0100

    E-mail notification localization en, it

---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+	'greeting_hello' => 'Hello!',
+	'greeting_error' => 'Whoops!',
+
+	'regards' => 'Regards',
+
+	'action_message' => 'If you’re having trouble clicking the :action_text button, copy and paste the URL below into your web browser:',
+
+];
---
+++ resources/lang/it/notifications.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+	'greeting_hello' => 'Ciao!',
+	'greeting_error' => 'Oops!',
+
+	'regards' => 'A presto',
+
+	'action_message' => 'Se non riesci a cliccare sul pulsante :action_text button, copia l\'indirizzo sottostante e incollala nella barra degli indirizzi del tuo browser web:',
+
+];
