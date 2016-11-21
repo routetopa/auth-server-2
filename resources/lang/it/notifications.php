c78ecfe23846a89eb3f93ef2fd9950097adff14e
Luca Vicidomini <lvicidomini@unisa.it>
2016-11-21 13:34:39 +0100

    Localization it typos

--- resources/lang/it/notifications.php
+++ resources/lang/it/notifications.php
@@ -7,10 +7,10 @@ return [
 
 	'regards' => 'A presto',
 
-	'action_message' => 'Se non riesci a cliccare sul pulsante :action_text button, copia l\'indirizzo sottostante e incollala nella barra degli indirizzi del tuo browser web:',
+	'action_message' => 'Se non riesci a cliccare sul pulsante :action_text, copia l\'indirizzo sottostante e incollala nella barra degli indirizzi del tuo browser web:',
 
 	'reset' => [
-		'line1' => 'Ricevi questo messaggio perché qualcuno ha chiesto di reimpostare las password per il tuo account.',
+		'line1' => 'Ricevi questo messaggio perché qualcuno ha chiesto di reimpostare la password per il tuo account.',
 		'line2' => 'Se non hai chiesto tu di reimpostare la password, puoi semplicemente ignorare questo messaggio.',
 		'action_button' => 'Reimposta password',
 	],
